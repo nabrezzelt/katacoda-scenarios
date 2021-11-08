a7041231423014c849e4f0428c42459d9daba00c
nabrezzelt <nabrezzelt@web.de>
2021-11-08 12:58:20 +0100

    Added volume mouting to init

--- import-export/init-environment.sh
+++ import-export/init-environment.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --name postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d postgres
+docker run --name postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d -v /root:/data postgres
 docker exec -it postgres bash
