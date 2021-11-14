5bedd01a7045a636096f03d5bbaabb17bb1f4a1b
nabrezzelt <nabrezzelt@web.de>
2021-11-14 12:49:54 +0100

    add sleep

--- import-export/init-environment.sh
+++ import-export/init-environment.sh
@@ -3,6 +3,8 @@
 docker run --name postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d -v /root:/data postgres
 docker exec -it postgres bash
 
+sleep(1)
+
 psql -U postgres -f /data/person-data.sql
 
 psql -U postgres
