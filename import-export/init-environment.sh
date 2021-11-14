574b3e4ee1d7757a12dc49971c83a13b3dfea7c2
nabrezzelt <nabrezzelt@web.de>
2021-11-14 12:49:04 +0100

    removed echos

--- import-export/init-environment.sh
+++ import-export/init-environment.sh
@@ -3,8 +3,6 @@
 docker run --name postgres -e POSTGRES_PASSWORD=postgres -p 5432:5432 -d -v /root:/data postgres
 docker exec -it postgres bash
 
-echo Loading data...
 psql -U postgres -f /data/person-data.sql
 
-echo Loading finished...
 psql -U postgres
